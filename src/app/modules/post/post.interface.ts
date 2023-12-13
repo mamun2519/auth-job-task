c63d2421dbfb4692ec0a69178ce2659207ffa0ab
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-12-14 00:11:40 +0600

    add to getall post or delete post api

--- src/app/modules/post/post.interface.ts
+++ src/app/modules/post/post.interface.ts
@@ -8,20 +8,12 @@ export type IPost = {
   imgUrl: string;
   description: string;
   like: number;
-  likedUser: [{ user: IUser | ObjectId }];
-  commentedUser: [{ user: IUser | ObjectId; comment: string }];
+  likes: {
+    user: ObjectId | IUser | any;
+  }[];
+  comments: {
+    user: ObjectId | IUser | any;
+    comment: string;
+  }[];
 };
 export type PostModel = Model<IPost, Record<string, unknown>>;
-//Post Like Interface
-export type IPostLike = {
-  user: IUser | ObjectId;
-  post: IPost | ObjectId;
-};
-export type PostLikeModel = Model<IPostLike, Record<string, unknown>>;
-//Post Comment Interface
-export type IPostComment = {
-  user: IUser | ObjectId;
-  post: IPost | ObjectId;
-  comment: string;
-};
-export type PostCommentModel = Model<IPostComment, Record<string, unknown>>;
