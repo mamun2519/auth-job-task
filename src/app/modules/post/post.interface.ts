663524d52d853174d974c2edbce87ac53d9408f9
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-12-13 23:09:48 +0600

    add to Post like or post comment inteface

--- src/app/modules/post/post.interface.ts
+++ src/app/modules/post/post.interface.ts
@@ -2,10 +2,23 @@ import { Model, ObjectId } from "mongoose";
 import { IUser } from "../auth/auth.interface";
 // Post interface
 export type IPost = {
-  userId: IUser | ObjectId;
+  user: IUser | ObjectId;
   title: string;
   imgUrl: string;
   description: string;
   like: number;
 };
 export type PostModel = Model<IPost, Record<string, unknown>>;
+//Post Like Interface
+export type IPostLike = {
+  user: IUser | ObjectId;
+  post: IPost | ObjectId;
+};
+export type PostLikeModel = Model<IPostLike, Record<string, unknown>>;
+//Post Comment Interface
+export type IPostComment = {
+  user: IUser | ObjectId;
+  post: IPost | ObjectId;
+  comment: string;
+};
+export type PostCommentModel = Model<IPostComment, Record<string, unknown>>;
