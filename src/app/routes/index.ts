dd9ac4b78493f196f1c4d3ecd3ffc6b0c4a75632
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-12-14 00:02:53 +0600

    add to get all api route

--- src/app/routes/index.ts
+++ src/app/routes/index.ts
@@ -1,11 +1,15 @@
 import express from "express";
 
 import { AuthRouter } from "../modules/auth/auth.route";
+import { PostRoute } from "../modules/post/post.route";
 
 const router = express.Router();
 
 // all module route is here
-const AllModuleRoutes = [{ path: "/auth", router: AuthRouter }];
+const AllModuleRoutes = [
+  { path: "/auth", router: AuthRouter },
+  { path: "/post", router: PostRoute },
+];
 
 AllModuleRoutes.forEach((route) => router.use(route.path, route.router));
 
