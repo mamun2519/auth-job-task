925ebea8382fd321650b03b1c579b61803c08e30
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-12-14 00:02:02 +0600

    add to post router include all route

---
+++ src/app/modules/post/post.route.ts
@@ -0,0 +1,10 @@
+import express from "express";
+import { PostController } from "./post.controller";
+
+const router = express.Router();
+router.get("/:id", PostController.postById);
+router.delete("/:id", PostController.deletePostById);
+router.patch("/:id", PostController.updatePostById);
+router.post("/", PostController.insertPost);
+router.post("/", PostController.getAllPost);
+export const PostRoute = router;
