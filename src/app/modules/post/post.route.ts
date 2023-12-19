857db7ac23f86ee8ff3417d5df7989515d634fbd
Juboraj Islam Mamun <juborajvai22@gmail.com>
2023-12-19 12:15:47 +0600

    add to auth middleware

--- src/app/modules/post/post.route.ts
+++ src/app/modules/post/post.route.ts
@@ -4,11 +4,11 @@ import { AuthGard } from "../../middleware/authGard";
 
 const router = express.Router();
 router.get("/my-post", AuthGard(), PostController.myAllPost);
-router.get("/:id", PostController.postById);
-router.delete("/:id", PostController.deletePostById);
-router.patch("/:id", PostController.updatePostById);
+router.get("/:id", AuthGard(), PostController.postById);
+router.delete("/:id", AuthGard(), PostController.deletePostById);
+router.patch("/:id", AuthGard(), PostController.updatePostById);
 router.post("/", AuthGard(), PostController.insertPost);
-router.get("/", PostController.getAllPost);
+router.get("/", AuthGard(), PostController.getAllPost);
 router.post("/like", AuthGard(), PostController.postLike);
 router.post("/comment", AuthGard(), PostController.postComment);
 export const PostRoute = router;
